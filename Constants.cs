ab164edec79534ffa2a6972b21c55e64ba0d6119
John Wang (AUX) <johnwa@microsoft.com>
2019-04-13 15:38:40 -0700

    fix the source codes issue

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace reactcore
+{
+    public static class Constants
+    {
+        public static string AuthorizationCookieKey => "Auth";
+        public static string HttpContextServiceUserItemKey => "ServiceUser";
+    }
+}
